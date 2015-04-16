06ab34f4290c14644c283064e71a5d289b954446
davilloni <aristodemo.c@gmail.com>
2015-04-16 08:56:49 +0200

    add toClient method on userSchema

--- db/schemas/user.js
+++ db/schemas/user.js
@@ -1,4 +1,5 @@
-var mongoose    = require('mongoose'),
+var _           = require('lodash'),
+    mongoose    = require('mongoose'),
     Schema      = mongoose.Schema,
     bcrypt      = require('bcrypt'),
     SALT_FACTOR = 10;
@@ -15,6 +16,11 @@ userSchema.methods.comparePassword = function(ricerca, done) {
     bcrypt.compare(ricerca, this.password, function(err, isMatch) {
         done(err, isMatch);
     });
+};
+
+userSchema.methods.toClient = function() {
+    var user = _.pick(this, ['id', 'name']);
+    return user;
 };
 
 userSchema.pre('save', function(next) {
